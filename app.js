4c5763ab088d3353de5e2ea2f871ee3c1ee3d748
HARSHVARDHAN SOLANKI <solankiharsh1018@gmail.com>
2026-06-09 14:11:13 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const jwt = require('jsonwebtoken')
+const userModel = require('./models/user')
+const bcrypt = require('bcrypt')
+
+
+app.use( express.json() )
+app.use(express.urlencoded({extended:true}))
+app.set('view engine' , 'ejs')
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/' , (req,res)=>{
+    res.render('index')
+
+})
+
+app.post('/create' , (req,res)=>{
+    let {username , email, password , age} = req.body 
+
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt,async (err,hash)=>{
+            let user = await userModel.create({
+                username,
+                email,
+                password:hash,
+                age
+            })
+
+            let token = jwt.sign({email} , "secret")
+            res.cookie("token" , token)
+             res.render('created',{user})
+        })
+       
+    })
+
+})
+
+
+app.get('/logout' , (req,res)=>{
+    res.cookie("token" , "")
+    res.redirect('/')
+})
+
+app.get('/login' , (req,res)=>{
+    res.render('login')
+})
+
+app.post('/login' , async (req,res)=>{
+    let user = await userModel.findOne({email:req.body.email})
+    if(!user){
+        res.render('no')
+    }
+    bcrypt.compare(req.body.password , user.password , (err,result)=>{
+        
+        if(result){
+            let token = jwt.sign({email:user.email} , "secret")
+            res.render('yes',{user})
+        }
+        else{
+            res.render('no')
+        }
+    })
+
+})
+
+
+app.listen(3000)
